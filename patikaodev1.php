fc318295ee3521250b41e227e90c18f033c44947
canaksakal <76426772+canaksakal@users.noreply.github.com>
2022-07-01 13:48:36 +0300

    first commit

---
+++ patikaodev1.php
@@ -0,0 +1,17 @@
+<?php
+echo "Patika Php Odev1";
+echo "<br>";
+
+    function ucgen($say){
+        $i=0;
+for($i;$i<=$say;$i++){
+        $j=0;
+        while($j<=$i){
+            $j++;
+            echo " 0";
+        }
+    echo "<br>";
+    }
+}
+    ucgen(15);
+?>
